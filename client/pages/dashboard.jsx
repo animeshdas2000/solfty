fa05ff6ed88da0d2a7bb713494c290ee2b14d67a
Anmol Choudhary <3anmol3@gmail.com>
2022-11-19 19:39:18 +0530

    frontend

---
+++ client/pages/dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { css } from '@emotion/css'
+// import '../styles/dashboard.css' 
+import bg from '../assets/bg.jpg'
+
+function dashboard() {
+  return (
+    <main className="landing" >
+        <h1>Your Go to Platform for Decenterlize Ticketing</h1> 
+        <button>Get Started</button>
+
+    </main>
+  )
+}
+
+export default dashboardt
